6c73b33e3e5f928bf82931de5eff12537590aaae
ajdonich <ajdonich@gmail.com>
2025-06-19 19:25:27 -0700

    feat: assignment9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = a87a52bcd382c09ae367bc8959710b95da1fbb53 
+AESD_ASSIGNMENTS_VERSION = 36d14ebf22aca596afcf26f07f08a236cfb6720d 
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-ajdonich.git
 #AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/ubuntu/assignments-3-and-later-ajdonich
 AESD_ASSIGNMENTS_SITE_METHOD = git
@@ -26,7 +26,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder* $(@D)/finder-app/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment9-buildroot/* $(TARGET_DIR)/bin
 endef
 
 $(eval $(kernel-module))
